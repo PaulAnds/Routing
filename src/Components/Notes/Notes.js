7e777362ab7cfaf6f5fac05a0cde2c916bd2d775
PaulAnds <73005395+PaulAnds@users.noreply.github.com>
2022-06-17 13:43:06 -0500

    class

--- src/Components/Notes/Notes.js
+++ src/Components/Notes/Notes.js
@@ -22,33 +22,27 @@ const Notes = () => {
     const selectedNote = filterNotes(noteId);
 
     const renderNote = (notes) => (
-            <ul>
-                {
-                    notes.map((note,key) => (
-                        <li key = {key}>
-                            <Link to={`/notes/${note.id}`}>{note.button}</Link>
-                            {sendTitle(note)}
-                            {sendDescription(note)}
-                        </li>
-                    ))
-                }
-            </ul>
+        <ul>
+            {
+                
+                notes.map((note, key) => (
+                    <li key={key}>
+                        
+                        {
+                            (selectedNote)?
+                            <div>
+                                <h2>{note.title}</h2>
+                                <p>{note.description}</p>
+                            </div>
+:
+                            <Link to={`/notes/${note.id}`}>{note.title}</Link>
+                        }
+
+                    </li>
+                ))
+            }
+        </ul> 
     )
-
-    const sendTitle = (note) => {
-        if(selectedNote){
-            return <h3>{note.title}</h3>
-        }
-        
-    }
-
-    const sendDescription = (note) => {
-        if(selectedNote){
-            return <h3>{note.description}</h3>
-        }
-        
-    }
-
     return (
         <div className="Notes">
             <h1>Notes</h1>
